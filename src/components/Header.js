83fa320583733ffd4e7750eba91e0e2f79e35b4d
Rennan <reiony.dev@gmail.com>
2022-10-03 17:48:20 -0300

    F

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,6 @@ const LogoContainer = styled.div`
   align-items: center;
   margin: 40px 0 20px 0;
   h1{
-    @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
     font-family: 'Righteous';
     font-style: normal;
     font-weight: 400;
